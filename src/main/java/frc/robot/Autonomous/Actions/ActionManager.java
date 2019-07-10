49151f56f697b792f1479d0952864d2f9960101b
Zach LeClaire <33380765+zachleclaire6561@users.noreply.github.com>
2019-07-10 14:11:10 -0700

    updating ActionManager
    yee

--- src/main/java/frc/robot/Autonomous/Actions/ActionManager.java
+++ src/main/java/frc/robot/Autonomous/Actions/ActionManager.java
@@ -1,64 +1,39 @@
 package frc.robot.Autonomous.Actions;
 
-import java.util.LinkedList;
-import frc.robot.tasks.Loop;
+import java.util.
 
-public class ActionManager{
+public class ThreadedManager implements Runnable{
  
-  private LinkedList<Action> queue = new LinkedList<Action>();
-  private int currentIndex;
-  private boolean startOfAction;
 
-    Loop loop = new Loop(){
-        @Override
-        public void onStart(double timeStamp){
-            currentIndex = 0;
-        }
-        @Override
-        public void onLoop(double timeStamp){
-            Action action = queue.get(currentIndex);
-            if(startOfAction){
-               action.start(timeStamp);
-            }
-            else{
-                if(action.isDone())
-                   action.stop(timeStamp);
-                   if(currentIndex + 1 < queue.size()){
-                        currentIndex ++;
-                   }
-                   else{
-                       // DO NOTHING BOIS WE GUCCI
-                       System.out.println("you forgot to do a ");
-                   }
-                if(action.isActive()){
-                    if(!action.isOK()){
-                        // i don't feel so good
-                        action.failCondition(timeStamp);
-                    }
-                    else{
-                       //thank you, next
-                       action.loop(timeStamp);
-                    }
-                }
-                
-            }
-        }
-        @Override
-        public void onStop(double timeStamp){
+  private Vector<ThreadBoi> queueThreaded;
+  private boolean isRunning;
+  private boolean isPaused;
+  private double lastTimeStamp;
+  private ExecutorService executor;
+ 
+   public ThreadedManager(){
+      executor = Executors.newSingleThreadExecutor();
+      isRunning = true;
+      isPaused = true;
+   }
+ 
+ @Override 
+ public void run(){
+ 
+ }
+ 
+  void addToQueue(ThreadBoi){
+    queueThreaded.add(task);
+  }
+  
+ public void pause(){
+  isPaused = true;
+ }
+ 
+ public void unPause(){
+  isPaused = false;
+ }
+ 
 
-        }
-
-    };
-
-    public synchronized void addToQueue(Action action){
-        queue.add(action);
-    }
-
-    public Action getNext(){
-        return queue.getLast();
-    }
-
-    public Action getCurrentName(){
-        return queue.element();
-    }
+   
 }
